760882642fbd0a388fc892ebf28227688c6e42b8
Timi <timi@helloworld.ng>
2016-06-23 12:27:29 +0100

    Changed routes for auth to have /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: '/auth', controllers: { omniauth_callbacks: 'overrides/omniauth_callbacks'}
-
+  mount_devise_token_auth_for 'User', at: '/api/auth', controllers: { omniauth_callbacks: 'overrides/omniauth_callbacks'}
   namespace :api, defaults: {format: :json} do
     scope module: :v1 do
       resources :users, only: [:show]
