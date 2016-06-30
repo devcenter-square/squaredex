e66a493fd20c89165df1193751eb1bf942cfccd9
Timi <timi@helloworld.ng>
2016-06-30 11:24:27 +0100

    Added /#/ to redirect url for angular.

--- app/controllers/overrides/omniauth_callbacks_controller.rb
+++ app/controllers/overrides/omniauth_callbacks_controller.rb
@@ -2,7 +2,7 @@ module Overrides
   class OmniauthCallbacksController < DeviseTokenAuth::OmniauthCallbacksController
 
     def auth_origin_url
-      'https://squaredex.herokuapp.com/auth-return'
+      'https://squaredex.herokuapp.com/#/auth-return'
     end
 
   end
