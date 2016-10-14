641d3a3ae1c141a503bdf093f9038c5e08205c6b
Sunday Adefila <adefilaedward@gmail.com>
2016-10-15 00:52:10 +0100

    create admin? method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
 
   validates :role, inclusion: ROLES
 
+  def admin?
+    role == 'admin'
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,16 @@ describe User do
 
   it { should validate_inclusion_of(:role).in_array(User::ROLES) }
 
+  describe '.admin?' do
+    it 'is true for an admin user' do
+      admin = build(:user, :admin)
+      expect(admin.admin?).to be_truthy
+    end
+
+    it 'is false for a non admin user' do
+      user = build(:user)
+      expect(user.admin?).to be_falsey
+    end
+  end
+
 end
