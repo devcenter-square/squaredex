f64247aec3771ed070a0607eeefa93d8b7111e16
Sunday Adefila <adefilaedward@gmail.com>
2016-10-14 23:35:10 +0100

    add spec for unauthorised and authorised request to the karma management controller. WIP

--- spec/controllers/karma_management_controller_spec.rb
+++ spec/controllers/karma_management_controller_spec.rb
@@ -16,4 +16,30 @@ describe Api::V1::KarmaManagementController, type: :controller do
 
   end
 
+  describe 'unauthorized' do
+    before do
+      sign_in user
+    end
+
+    describe 'GET#index' do
+      it 'has restricted access' do
+        get :index
+        expect(response.status).to eq 401
+      end
+    end
+  end
+
+  describe 'authorized' do
+    before do
+      sign_in admin
+    end
+
+    describe 'GET#index' do
+      it 'has a success response status' do
+        get :index
+        expect(response.status).to eq 200
+      end
+    end
+  end
+
 end
