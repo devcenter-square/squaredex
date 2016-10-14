7da0bd3a61990c258efbc4097c3a7206ee8559a1
Sunday Adefila <adefilaedward@gmail.com>
2016-10-14 10:52:28 +0100

    add a ruby version and fix bug in db cleaner file

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,23 @@
-config.before(:suite) do
-  DatabaseCleaner.clean_with(:truncation)
-end
+RSpec.configure do |config|
 
-config.before(:each) do
-  DatabaseCleaner.strategy = :transaction
-end
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
 
-config.before(:each, :js => true) do
-  DatabaseCleaner.strategy = :truncation
-end
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
 
-config.before(:each) do
-  DatabaseCleaner.start
-end
+  config.before(:each, :js => true) do
+    DatabaseCleaner.strategy = :truncation
+  end
 
-config.after(:each) do
-  DatabaseCleaner.clean
-end
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
 
 end
